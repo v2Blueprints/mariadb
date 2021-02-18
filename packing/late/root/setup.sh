320fe339c5c1503cf9ca8c4431a067b9229e9140
James Vodanovich <james@jvodan.com>
2021-02-18 14:20:16 +1100

    fix setup script

--- packing/late/root/setup.sh
+++ packing/late/root/setup.sh
@@ -5,9 +5,12 @@ service mysql status |grep stopped >/dev/null
 if test $? -eq 0
  then
    mysql_install_db 
-   mysql < /root/init.sql
+   
+   service mysql start 
+   sleep 5
+   
+   mysql -h 127.0.0.1 < /root/init.sql
    rm /root/init.sql
-   service mysql start
  else
    	mysql_upgrade
  fi    
