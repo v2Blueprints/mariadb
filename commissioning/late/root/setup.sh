03b16d5f8e30d0a95ececd40ad63c5aea2f0dd83
James Vodanovich <james@jvodan.com>
2021-03-16 14:25:14 +1100

    commisioning handles a recreate container by applying mysql_upgrade
    Prior it blindly ran install

--- commissioning/late/root/setup.sh
+++ commissioning/late/root/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-mysql_install_db 
-mysql < /tmp/packing/early/init.sql
 
+if ! test -f /var/lib/mysql/libdata
+	 then
+		mysql_install_db 
+		mysql < /tmp/packing/early/init.sql
+	 else
+	 	 mysql_upgrade -u rma
+fi
