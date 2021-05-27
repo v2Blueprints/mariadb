57a20e5c0f02f1cc5fe487b5c310d79f689d8642
James Vodanovich <james@jvodan.com>
2021-05-27 13:51:46 +1000

    add output to aid in debug

--- commissioning/late/root/setup.sh
+++ commissioning/late/root/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-if ! test -f /var/lib/mysql/libdata
+if ! test -f /var/lib/mysql/libdata1
 	 then
+	 	echo installing initial database
 		mysql_install_db 
 		mysql < /tmp/packing/early/init.sql
 	 else
+	 	 echo Performing database upgrade
 	 	 mysql_upgrade -u rma
 fi
