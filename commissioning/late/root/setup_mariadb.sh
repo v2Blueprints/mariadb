eb5ef276cd1da88e099658f4b8439207902a7dc9
James Vodanovich <james@jvodan.com>
2021-07-02 09:36:50 +1000

    common setup script

---
+++ commissioning/late/root/setup_mariadb.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if ! test -f /var/lib/mysql/libdata1
+	 then
+	 	echo installing initial database
+		mysql_install_db 
+		mysql < /tmp/packing/late/init.sql
+	 elif ! test -f /var/run/created
+	 	 echo Performing database upgrade
+	 	 mysql_upgrade -u rma	 	 
+fi
+
+touch /var/run/created
--- packing/early/usr/local/bin/start.sh
+++ packing/early/usr/local/bin/start.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-if ! test -f /var/lib/mysql/libdata1
-	 then
-	 	echo installing initial database
-		mysql_install_db 
-		mysql < /tmp/packing/late/init.sql
-	 else
-	 	 echo Performing database upgrade
-	 	 mysql_upgrade -u rma
-fi
-
-
+/root/setup_mariadb.sh
 exec mysqld
