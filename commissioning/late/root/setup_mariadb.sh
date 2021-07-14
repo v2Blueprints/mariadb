b3388cf13a12914ff81edc0f5b096e98ee8bdce1
James Vodanovich <james@jvodan.com>
2021-07-14 13:49:53 +1000

    set correct permission on /var/run/mysql dri

--- commissioning/late/root/setup_mariadb.sh
+++ commissioning/late/root/setup_mariadb.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 mkdir -p /var/run/mysqld/
-rm /var/run/mysqld/mysqld.sock
-if ! test -f /var/lib/mysql/libdata1
+chown mysql /var/run/mysqld/
+
+	if ! test -f /var/lib/mysql/libdata1
 	 then
 	 	echo installing initial database
 		mysql_install_db 
