b80f46434bdcbbbadbe5353eeac76cc30024dff8
Mark Ratjens <mark@ratjens.com>
2021-12-14 14:14:10 +1100

    use interpolation to map status service entry path
    Signed-off-by: Mark Ratjens <mark@ratjens.com>

--- commissioning/late/root/setup_mariadb.sh
+++ commissioning/late/root/setup_mariadb.sh
@@ -6,13 +6,13 @@ chown mysql /var/run/mysqld/
 	if ! test -f /var/lib/mysql/libdata1
 	 then
 	 	echo installing initial database
-		mysql_install_db 
+		mysql_install_db
 		mysql < /root/init.sql
-	 elif ! test -f /var/run/created
+	 elif ! test -f ^^service_path^^/startup_complete
 	 	 then
 	 	 echo Performing database upgrade
-	 	 mysql_upgrade -u rma	
-	 	  	 
+	 	 mysql_upgrade -u rma
+
 fi
 
-touch /var/run/status/startup_complete
+touch ^^service_path^^/startup_complete
