5415af7f5b3e44035ab2051285fa1b71a8bb9c52
Mark Ratjens <mark@ratjens.com>
2022-01-19 11:01:40 +1100

    change touch in start to use spaces-recognisable milestone

--- packing/early/usr/local/bin/start.sh
+++ packing/early/usr/local/bin/start.sh
@@ -3,6 +3,6 @@ ls -lRa /var/run
 ls -lRa /var/run/
 rm /var/run/mysqld/mysqld.sock
 /root/setup_mariadb.sh
-touch ^^service_path^^/startup_complete
+touch ^^service_path^^/startup
 exec mysqld
 sleep 120
